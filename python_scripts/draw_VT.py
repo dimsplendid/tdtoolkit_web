4f954c82ad9208fd136155a727e744fbda98852b
dimsplendid <dimsplendid@gmail.com>
2021-07-21 11:32:54 +0800

    V-T curve query begin

---
+++ python_scripts/draw_VT.py
@@ -0,0 +1,13 @@
+import sys
+# import numpy as np
+# import pandas as pd
+
+params = {
+    "LC": sys.argv[1],
+    "cell_gap": sys.argv[2],
+    "V_max": sys.argv[3],
+    "V_min": sys.argv[4]
+}
+
+print(params)
+print('python finished')
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express from 'express';
-​
+import express from 'express'
+​import {PythonShell, Options} from 'python-shell'
+
 const app = express();
 const port = 3000;
 
@@ -11,6 +12,26 @@ app.get('/', (req, res) => {
     // res.send('The server is working!');
     res.sendFile('public/index.html', {root: '.'})
 });
+
+app.get('/plot/VT', (req, res) => {
+    let options = {
+        mode: "text",
+        scriptPath: './python_scripts',
+        args: [
+            req.query.LC,
+            req.query.cell_gap,
+            req.query.V_max,
+            req.query.V_min
+        ]
+    } as Options
+    PythonShell.run('draw_VT.py', options, (err, output) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(output)
+        }
+    })
+})
 
 // for insert vender measuring data
 // Something weird here, need figure out later
